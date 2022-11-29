aed4680ce408c1fe55b0e986bce2467b0483466d
MohammdAshik <mohammadashik797@gmail.com>
2022-11-29 17:06:33 +0600

    my booking route added

--- src/Pages/My Booking/MyBooking.jsx
+++ src/Pages/My Booking/MyBooking.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const MyBooking = () => {
-  return <div>this is from my booking</div>;
+  return (
+    <div>
+      <div className="sticky top-[-13px] bg-white">
+        <h2 className="text-2xl font-semibold mb-1">My Booking {}</h2>
+        <hr />
+        <label
+          htmlFor="dashboard-drawer"
+          className="btn btn-ghost drawer-button text-primary absolute top-[-8px] right-1 lg:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default MyBooking;
---
+++ src/Pages/Shared/NotAvailable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotAvailable = ({ children }) => {
+  return <div>this is from not NotAvailable page</div>;
+};
+
+export default NotAvailable;
